f68f8c8ec03857a1b41f9dcb85bef1e1090876a2
Enricko <enricko.putra028@gmail.com>
2023-12-11 19:47:26 +0700

    Login UI

--- lib/page/auth/login.dart
+++ lib/page/auth/login.dart
@@ -1,3 +1,4 @@
+import "package:flutter/gestures.dart";
 import "package:flutter/material.dart";
 
 class LoginPage extends StatefulWidget {
@@ -10,16 +11,215 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.symmetric(horizontal: 10),
-      child: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text("Login"),
-          ],
+    return SingleChildScrollView(
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 15),
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              // Image Asset Login
+              Image.asset(
+                "assets/man_computer.png",
+                height: 275,
+                width: 275,
+                fit: BoxFit.contain,
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+
+              // Form Section Login
+              SizedBox(
+                width: double.infinity,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      "Login",
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                        color: Color(0xFF36454F),
+                        fontSize: 26,
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 12,
+                    ),
+                    const Text(
+                      "Selamat Datang Kembali",
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                        color: Color(0xFF696F79),
+                        fontSize: 14,
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 12,
+                    ),
+                    Form(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            "Email",
+                            textAlign: TextAlign.left,
+                            style: TextStyle(
+                              color: Color(0xFF696F79),
+                              fontSize: 14,
+                              fontFamily: 'Poppins',
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 12,
+                          ),
+                          TextFormField(
+                            decoration: InputDecoration(
+                              filled: true,
+                              fillColor: const Color(0xFFFCFDFE),
+                              hintText: "Masukan Email Anda",
+                              hintStyle: const TextStyle(
+                                color: Color(0xFF696F79),
+                                fontSize: 14,
+                                fontFamily: 'Poppins',
+                                fontWeight: FontWeight.w400,
+                              ),
+                              prefixIcon: const Icon(Icons.email_outlined),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(8),
+                                borderSide: const BorderSide(
+                                    width: 1, color: Color(0xFFDEDEDE)),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 12,
+                          ),
+                          const Text(
+                            "Password",
+                            textAlign: TextAlign.left,
+                            style: TextStyle(
+                              color: Color(0xFF696F79),
+                              fontSize: 14,
+                              fontFamily: 'Poppins',
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 12,
+                          ),
+                          TextFormField(
+                            decoration: InputDecoration(
+                              filled: true,
+                              fillColor: const Color(0xFFFCFDFE),
+                              hintText: "Masukan Password Anda",
+                              hintStyle: const TextStyle(
+                                color: Color(0xFF696F79),
+                                fontSize: 14,
+                                fontFamily: 'Poppins',
+                                fontWeight: FontWeight.w400,
+                              ),
+                              prefixIcon: const Icon(Icons.lock_outline),
+                              suffixIcon: const Icon(Icons.visibility_outlined),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(8),
+                                borderSide: const BorderSide(
+                                    width: 1, color: Color(0xFFDEDEDE)),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+              Container(
+                alignment: Alignment.centerRight,
+                child: InkWell(
+                  onTap: () {},
+                  child: Text(
+                    "Lupa Password?",
+                    textAlign: TextAlign.right,
+                    style: TextStyle(
+                      color: Color(0xFF2FA4D9),
+                      fontSize: 16,
+                      fontFamily: 'Poppins',
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+              ),
+
+              const SizedBox(
+                height: 12,
+              ),
+              SizedBox(
+                width: double.infinity,
+                height: 50,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xFF2FA4D9),
+                  ),
+                  child: const Text(
+                    "Login",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Color(0xFFF6F7FB),
+                      fontSize: 16,
+                      fontFamily: 'Poppins',
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 12,
+              ),
+              RichText(
+                text: TextSpan(
+                  children: [
+                    TextSpan(
+                      text: "Belum punya akun? ",
+                      style: TextStyle(
+                        color: Color(0xFF374253),
+                        fontSize: 16,
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                    TextSpan(
+                      text: 'Daftar',
+                      style: TextStyle(
+                        color: Color(0xFF2FA4D9),
+                        fontSize: 16,
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.w400,
+                      ),
+                      recognizer: TapGestureRecognizer()
+                        ..onTap = () {
+                          print('Login Text Clicked');
+                        },
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
