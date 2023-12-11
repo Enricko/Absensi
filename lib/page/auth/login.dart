30512c82b5fd479449476cf49d9ea36dfeb53b2d
Enricko <enricko.putra028@gmail.com>
2023-12-11 18:43:39 +0700

    Image & Login (in Progress)

---
+++ lib/page/auth/login.dart
@@ -0,0 +1,27 @@
+import "package:flutter/material.dart";
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 10),
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text("Login"),
+          ],
+        ),
+      ),
+    );
+  }
+}
